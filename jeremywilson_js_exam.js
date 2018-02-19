86d99dcbf76238c7e9741ea321dd0ad2a2231d3d
Jeremy Wilson <jeremybwilson@gmail.com>
2018-02-19 12:39:59 -0800

    Deleted some files

--- jeremywilson_js_exam.js
+++ jeremywilson_js_exam.js
@@ -66,33 +66,24 @@ printUp(arr);
 CHALLENGE 2:
 // Print all values of the array
 
-function printArray(){
-  var arr = [];
+function printArray(arr){
   for (var i = 0; i < arr.length; i++){
-    // console.log(arr[i]);
-    return arr[i];
+    console.log(arr[i]);
   }
 }
-console.log();
-
-function printArray(){
-  var arr = [];
-  for(var i = 0; i < arr.length; i++){
-    arr += arr[i];
-  }
-  return arr;
-}
-console.log(printArray(['hello','world', 1,2,3]));
-
-printArray(['hello', 'world', 1,2,3]);
+printArray(['hello','world',1,2,3]);
 
 
 CHALLENGE 3:
 // Print only the values of the array that are over the value of 10
-function printOnlyOverValueTen(){
-  for (i < 0; i >= 10; i++) {
-    console.log(i);
+function printOnlyOverValueTen(arr){
+  var array = [];
+  for (var i = 1; i < arr.length; i++) {
+    if(arr[i] > 10){
+      array.push(arr[i]);
+    }
   }
+  return array;
 }
 printOnlyOverValueTen([1,3,5,8,10,12,14,16]);
 
@@ -105,8 +96,29 @@ printEven();
 BONUS CHALLENGE 5:
 // Print the min and max value of an array
 function minMax(arr){
-// …
+  var max = arr[0];
+  var min = arr[0];
+  for(var i = 1; i < arr.length; i++){
+    if(max < arr[i]){
+      max = arr[i];
+    }
+    if(min > arr[i]){
+      min = arr[i];
+    }
+  }
 console.log(min)
 console.log(max)
 }
 minMax([12,14,16,18,16,14,12]);
+
+// find and print only odd number values
+function oddNumbers(array) {
+  var array = [];
+  for(var i = 1; i < array.length; i++){
+    if(i % 2 !== 0){
+      array.push(i);
+    }
+  }
+  return array;
+}
+oddNumbers([1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25,26,27,28,29,30,31,32,33,34,35,36,37,38,39,40,41,42,43,44,45,46,47,48,49,50]);

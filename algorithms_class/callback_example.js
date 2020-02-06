a28b3b379857dbd1e5c82df538226e2fcd071b36
Jeremy Wilson <jeremy_b_wilson@hotmail.com>
2020-02-05 20:04:50 -0800

    removing algorithm files to new repo

--- algorithms_class/callback_example.js
+++
@@ -1,8 +0,0 @@
-function doHomeWork(subject, callback){
-  console.log(`Starting my ${subject} homework.`);
-  callback();
-}
-
-doHomeWork('math', function() {
-  console.log(`Finished my homework`);
-});
--- algorithms_class/square_the_values.js
+++
@@ -1,5 +0,0 @@
-// Square the values
-const squared = (a) => a * a;
-
-console.log(squared(5));  // Should output 25
-console.log(squared(10));  // Should output 100
